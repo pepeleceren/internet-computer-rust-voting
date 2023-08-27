090f8ebac22873ad07e87457c56d32631336c7c2
Ceren Pepele <cerenpepele@Ceren-Air.home>
2023-08-27 22:58:17 +0300

    Initial commit.

---
+++ src/voting_canister_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { voting_canister_backend } from "../../declarations/voting_canister_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await voting_canister_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
